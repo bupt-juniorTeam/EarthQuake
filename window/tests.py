1511523892692eb391d9969c368a4d32b7bf862d
buggyminer <liaojiudazhongbiao@outlook.com>
2021-06-03 13:17:59 +0800

    a fool learned how to usr url

--- window/tests.py
+++ window/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import resolve
+from window.views import home_page
 
-# Create your tests here.
+class URLTest(TestCase):
+    def test_homepage(self):
+        found=resolve('/window/')
+        print(found.url_name)
+        self.assertEqual(found.func,home_page)
