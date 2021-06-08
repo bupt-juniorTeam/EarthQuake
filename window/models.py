d3467ddef8c67943dbc90dd77ee19671647f3c0d
chui <1229059439@qq.com>
2021-06-08 23:26:26 +0800

    完成数据库的搭建

--- window/models.py
+++ window/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+from django.utils import timezone
+
+
+class Location(models.Model):
+    longitude = models.DecimalField(decimal_places=2, max_digits=5, default=0.0)
+    latitude = models.DecimalField(decimal_places=2, max_digits=5, default=0.0)
+
+
+class Resource(models.Model):
+    url = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+
+
+class EarthQuake(models.Model):
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, default=None)
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE, default=None)
+    time = models.DateTimeField(auto_now_add=True)
+    city = models.CharField(max_length=255)
+    magnitude = models.CharField(max_length=255)
--- window/tests.py
+++ window/tests.py
@@ -1,9 +1,21 @@
 from django.test import TestCase
 from django.urls import resolve
+from .models import *
 from window.views import home_page
+
 
 class URLTest(TestCase):
     def test_homepage(self):
-        found=resolve('/window/')
+        found = resolve('/window/')
         print(found.url_name)
-        self.assertEqual(found.func,home_page)
+        self.assertEqual(found.func, home_page)
+
+
+class ModelTest(TestCase):
+    def test_create_model(self):
+        location = Location.objects.create(longitude=22.35, latitude=123.11)
+        resource = Resource.objects.create(url="test", name="correct")
+        earthquake = EarthQuake.objects.create(
+            location=location, resource=resource, time=timezone.now(), city="233331100000", magnitude="特大"
+        )
+        self.assertEqual(EarthQuake.objects.count(), 1)
